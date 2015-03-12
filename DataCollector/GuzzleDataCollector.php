c1772e0480376df7c4efc8962485bcdcc6adb320
Denis Zunke <denis.zunke@googlemail.com>
2015-03-12 15:32:17 +0100

    adding port to datacollector and output

--- DataCollector/GuzzleDataCollector.php
+++ DataCollector/GuzzleDataCollector.php
@@ -134,6 +134,7 @@ class GuzzleDataCollector extends DataCollector
             'method'  => $request->getMethod(),
             'scheme'  => $request->getScheme(),
             'host'    => $request->getHost(),
+			'port'    => $request->getPort(),
             'path'    => $request->getPath(),
             'query'   => $request->getQuery(),
             'body'    => $body
